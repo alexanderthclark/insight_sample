716ced84751bcbabfd08657cbc6e1159828a4783
alexanderthclark <alexanderthclark@users.noreply.github.com>
2018-10-01 13:01:56 -0400

    tobit regression

---
+++ tobit_reg.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import tobit 
+
+#tobit from https://github.com/jamesdj/tobit/blob/master/tobit.py
+
+
+
+#read data from data_assembly.py
+df = pd.read_csv('master_df.csv')
+
+#from feature_selection
+features = ['TEAGE', 't120303', 'PMFRM', 'PM25S', 'PMnonFRM', 'Ozone', 'NO2', 'SO2', 'CO',
+ 'PM10','PEEDUCA', 'Temperature','Temp_x_Age']
+
+##add interactions to df
+df['Temp_x_Age'] = df['Temperature']*df['TEAGE']
+
+##prepare data for regression
+x = df[features]
+
+#predict sleeplessness
+y = df['t010102']
+
+
+
+#Per tobit.py from jamesdj
+
+#Sleeplessness is censored at zero
+censored_at=0
+
+cens = pd.Series(np.zeros((len(y),)))
+cens[y==censored_at] = -1
+cens.value_counts()
+
+#Run a tobit regression
+tr = tobit.TobitModel()
+tr = tr.fit(x, y, cens, verbose=False)
+
+#print(tr.coef_)
+
+
+
+
+#coefficients!
+list( zip(features,list(tr.coef_)) )
+
